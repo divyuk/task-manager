babebb37136ee5187cc4b74090d3fe23b113078b
divyuk <div14kes@gmail.com>
2023-09-29 00:02:44 +0530

    Delete task

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -3,6 +3,8 @@ class Validator {
     if (
       task.hasOwnProperty("title") &&
       task.hasOwnProperty("desciption") &&
+      task.title.trim() !== "" && // Check if title is not empty after trimming
+      task.description.trim() !== "" && // Check if description is not empty after trimming
       task.hasOwnProperty("flag")
     )
       return { status: true, message: "Task has been created" };
--- src/index.js
+++ src/index.js
@@ -36,6 +36,32 @@ app.post("/tasks", (req, res) => {
   } else res.status(400).send(validator.validation(userPostedTask).message);
 });
 
+app.put("/tasks/:id", (req, res) => {
+  const ID = req.params.id;
+  const updatedBody = req.body;
+  const updatedTask = taskDB.map((task) => {
+    if (task.id == ID) return { ...task, ...updatedBody };
+    else return task;
+  });
+  const jsonData = JSON.stringify(updatedTask);
+  const writePath = path.join(__dirname, "tasks.json");
+  fs.writeFile(writePath, jsonData, (err) => {
+    if (err) res.status(500).send("Something went wrong!!");
+    else res.status(201).send("Task Updated");
+  });
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const ID = req.params.id;
+  const filtredTask = taskDB.filter((task) => task.id != ID);
+  const jsonData = JSON.stringify(filtredTask);
+  const writePath = path.join(__dirname, "tasks.json");
+  fs.writeFile(writePath, jsonData, (err) => {
+    if (err) res.status(500).send("Something went wrong!!");
+    else res.status(204).send("Task Updated");
+  });
+});
+
 app.listen(PORT, (error) => {
   if (error) console.log("Something went wrong...");
   else console.log("Server started on the port 3000");
