7e539316b451cdbb8accee3d909845b4ac2f33b7
divyuk <div14kes@gmail.com>
2023-09-28 13:17:37 +0530

    get task by single id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const taskDB = require("../tasks.json");
 
 const app = express();
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("This is the homepage...");
+});
+
+app.get("/tasks", (req, res) => {
+  res.status(200).send(taskDB);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const userProvidedID = req.params.id;
+  const singleTask = taskDB.filter((obj) => obj.id == userProvidedID);
+  res.status(200).send(singleTask);
+});
 
 app.listen(PORT, (error) => {
   if (error) console.log("Something went wrong...");
