bbbd3d3841cbe85c1f825570cb883ae6db0c4b6c
divyuk <div14kes@gmail.com>
2023-10-02 19:05:07 +0530

    Final update

--- index.js
+++ index.js
@@ -13,24 +13,14 @@ app.use(express.json());
 
 const corsOptions = {
   origin: "http://localhost:5173",
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 express.urlencoded({ extended: false });
-// app.get("/", (req, res) => {
-//   res.status(200).send("This is the homepage...");
-// });
+
 app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
   res.status(200).send("This is the homepage...");
 });
 
