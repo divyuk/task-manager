3cc234db4254be21558729dd1c2127f76c89b9ec
divyuk <div14kes@gmail.com>
2023-10-02 13:02:37 +0530

    Updated over GET

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 express.urlencoded({ extended: false });
+// app.get("/", (req, res) => {
+//   res.status(200).send("This is the homepage...");
+// });
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   res.status(200).send("This is the homepage...");
 });
 
